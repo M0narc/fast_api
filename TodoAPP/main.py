62190343a707da8df9fc01f3c23e7eb5d3a31672
Franco Lujan <francomatiaslujan23@gmail.com>
2024-02-22 18:56:58 -0300

    created the db connector for mysqlite and read_all endpoint

--- TodoAPP/main.py
+++ TodoAPP/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    """
+    db dependency
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get('/')
+async def read_all(db: db_dependency):
+    """
+    endpoint to get all Todos from the db
+    """
+    return db.query(Todos).all()
