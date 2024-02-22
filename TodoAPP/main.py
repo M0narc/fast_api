365f1d6ae2e133d11cbb40c00893cc1a1083b9e3
Franco Lujan <francomatiaslujan23@gmail.com>
2024-02-22 20:37:50 -0300

    data validation for get todos by id

--- TodoAPP/main.py
+++ TodoAPP/main.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
+
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -24,7 +26,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     """
     endpoint to get all Todos from the db
@@ -32,8 +34,8 @@ async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
 
-@app.get('/todo/{todo_id}')
-async def read_todo_by_id(db: db_dependency, todo_id: int):
+@app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+async def read_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
