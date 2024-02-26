b3bd6f591207289b99409e2ec679e91bfcdbc9bc
Franco Lujan <francomatiaslujan23@gmail.com>
2024-02-26 09:42:48 -0300

    post endpoint for creatign todos

---
+++ TodoAPP/todos_request.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'Todo Title',
+                'description': 'a brief description if possible',
+                'priority': 5,
+                'complete': False
+            }
+        }
