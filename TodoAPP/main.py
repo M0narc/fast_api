760642b5e7c7680b70b7ad648cd04ca296916c2f
Franco Lujan <francomatiaslujan23@gmail.com>
2024-02-26 10:07:28 -0300

    path data verification for update_todo

--- TodoAPP/main.py
+++ TodoAPP/main.py
@@ -60,8 +60,8 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
 
 @app.put('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db:db_dependency,
-                      todo_id: int,
-                      todo_request: TodoRequest):
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)):
     """
     endpoint to update todo by id
     """
