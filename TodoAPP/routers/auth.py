57a5ecf0d832155df5eb5e19df9ebb4e170a15ec
Franco Lujan <francomatiaslujan23@gmail.com>
2024-04-07 16:56:13 -0300

    bcrypt & passlib libraries installed to hash password in create_user endpoint

--- TodoAPP/routers/auth.py
+++ TodoAPP/routers/auth.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 from users_requests import CreateUserRequests
 from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 @router.post("/auth/")
@@ -10,6 +13,7 @@ async def create_users(create_user_request: CreateUserRequests):
     """
     endpoint to create users, we will not be unpackig **create_user_request
     since we can't, we have a password right now in our models
+    we are hashing the password using bcrypt
     """
     create_user_model = Users(
         email=create_user_request.email,
@@ -17,7 +21,7 @@ async def create_users(create_user_request: CreateUserRequests):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
 
