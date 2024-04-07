b82fbad002b7175597e8d72c79787f0551e01cd7
Franco Lujan <francomatiaslujan23@gmail.com>
2024-04-07 16:20:52 -0300

    users models, auth router and users requests for pydantics created

--- TodoAPP/routers/auth.py
+++ TodoAPP/routers/auth.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
+from users_requests import CreateUserRequests
+from models import Users
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+@router.post("/auth/")
+async def create_users(create_user_request: CreateUserRequests):
+    """
+    endpoint to create users, we will not be unpackig **create_user_request
+    since we can't, we have a password right now in our models
+    """
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+
+    return create_user_model
---
+++ TodoAPP/users_requests.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+
+class CreateUserRequests(BaseModel):
+    username: str = Field(min_length=8)
+    email: str
+    first_name: str
+    last_name: str
+    password: str = Field(min_length=8)
+    role: str
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'username': 'randomUserName',
+                'email': 'test@gmail.com',
+                'first_name': 'Random',
+                'last_name': 'Name',
+                'password': 'Test123',
+                'role': 'client'
+            }
+        }
